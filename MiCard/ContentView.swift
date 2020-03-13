47216a703ec994e2687dd3ab8115b8755d3f298e
Lalith <lalithpunepalli@gmail.com>
2020-03-13 16:41:50 +0530

    Initial Commit

---
+++ MiCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiCard
+//
+//  Created by Lalith on 13/03/20.
+//  Copyright © 2020 NANI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
